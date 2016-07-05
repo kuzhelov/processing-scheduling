760f921f14aaabf217213809880d13076e12b9d1
kuzhelov <deckster@mail.ru>
2016-07-05 04:36:44 +0300

    Add project files.

---
+++ ActionsProcessingScheduling/LimitedConcurrencyActionsScheduler.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActionsProcessingScheduling
+{
+	// the lack of .NET >= 4.5 features (such as awaitable calls) is intentional
+	public class LimitedConcurrencyActionsScheduler
+	{
+		public LimitedConcurrencyActionsScheduler(int maxConcurrencyLevel)
+		{
+			Debug.Assert(maxConcurrencyLevel > 0);
+
+			MaxConcurrencyLevel = maxConcurrencyLevel;
+		}
+
+		// has a promise-return semantics that provides an ability to 
+		// keep track of action execution progress and its result (in a sence of absence of errors)
+		public Task ScheduleAction(Action action, bool withHighestPriority = false)
+		{
+			var newActionDescriptor = new Tuple<Action, TaskCompletionSource<int>>(
+				item1: action,
+				item2: new TaskCompletionSource<int>());
+
+			lock (_schedulingSync)
+			{
+				if (withHighestPriority)
+				{
+					_pendingTasks.Insert(
+						index: 0,
+						item: newActionDescriptor);
+				}
+				else
+				{
+					_pendingTasks.Add(newActionDescriptor);
+				}
+
+				if (_currentlyProcessingJobsCount < MaxConcurrencyLevel)
+				{
+					++_currentlyProcessingJobsCount;
+					StartExecutionAsync(ofAction: action);
+				}
+			}
+
+			return newActionDescriptor.Item2.Task;
+		}
+
+		public int MaxConcurrencyLevel { get; private set; }
+
+		private void StartExecutionAsync(Action ofAction)
+		{
+			_taskFactory
+				.StartNew(() =>
+				{
+					// iterative approach is preferred to the recursive one
+					var shouldProcessNextTaskFromQueue = true;
+					while (shouldProcessNextTaskFromQueue)
+					{
+						shouldProcessNextTaskFromQueue = ProcessNextTaskFromQueue();
+					}
+				});
+		}
+
+		private bool ProcessNextTaskFromQueue()
+		{
+			Tuple<Action, TaskCompletionSource<int>> nextTaskDescriptor = null;
+
+			lock (_schedulingSync)
+			{
+				if (_pendingTasks.Any())
+				{
+					nextTaskDescriptor = _pendingTasks[0];
+					_pendingTasks.RemoveAt(0);
+				}
+				else
+				{
+					_currentlyProcessingJobsCount--;
+				}
+			}
+
+			if (nextTaskDescriptor != null)
+			{
+				var actionToProcess = nextTaskDescriptor.Item1;
+				var actionProcessingCompletionSource = nextTaskDescriptor.Item2;
+
+				try
+				{
+					actionToProcess();
+					actionProcessingCompletionSource.SetResult(0);
+				}
+				// ReSharper disable once EmptyGeneralCatchClause in order to posses consistency of actions processing (exceptions are swallowed)
+				catch (Exception exception)
+				{
+					actionProcessingCompletionSource.SetException(exception);
+				}
+
+				// recursive call here could be considered as an alternative, 
+				// but it is a bit more efficient and a way more safer to prefer the iterative approach instead
+				return true; 
+			}
+
+			return false;
+		}
+
+		private readonly TaskFactory _taskFactory = new TaskFactory();
+		private readonly List<Tuple<Action, TaskCompletionSource<int>>> _pendingTasks = 
+			new List<Tuple<Action, TaskCompletionSource<int>>>();
+
+		private readonly object _schedulingSync = new object();
+		private int _currentlyProcessingJobsCount;
+	}
+}
---
+++ ActionsProcessingScheduling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ActionsProcessingScheduling
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			// that's just the simple example of the way to guarantee that there is at least one processor 
+			// that stays free of processing even under the high load
+			var maxConcurrencyLevel = Math.Max(1, Environment.ProcessorCount - 1);
+
+			var scheduler = new LimitedConcurrencyActionsScheduler(
+				maxConcurrencyLevel: maxConcurrencyLevel);
+
+			// modern syntax constructs that are responsible for async calls are used for the sake of simplicity
+			// scheduler's implementation is completely unaware of these features intentionally
+			var successfulActions = new[]
+			{
+				CreateProcessingAction(completesSuccessfully: true, processingTime: TimeSpan.FromSeconds(3)),
+				CreateProcessingAction(completesSuccessfully: true, processingTime: TimeSpan.FromSeconds(1)),
+				CreateProcessingAction(completesSuccessfully: true, processingTime: TimeSpan.FromSeconds(5))
+			};
+
+			var failedActions = new[]
+			{
+				CreateProcessingAction(completesSuccessfully: false, processingTime: TimeSpan.FromSeconds(8)),
+				CreateProcessingAction(completesSuccessfully: false, processingTime: TimeSpan.FromSeconds(1))
+			};
+
+			Console.WriteLine($"Scheduling tasks with max concurrency level of: {maxConcurrencyLevel}");
+
+			var actionIdCounter = 0;
+
+			var tasksToWait = successfulActions.Concat(failedActions)
+				.Select(actionToProcess =>
+				{
+					var currentActionId = actionIdCounter++;
+
+					return scheduler.ScheduleAction(actionToProcess)
+						.ContinueWith(completedProcessingTask =>
+						{
+							Debug.Assert(completedProcessingTask.IsCompleted);
+							Console.WriteLine($"Action [{currentActionId}] has completed " +
+								(completedProcessingTask.IsFaulted ? "with error" : "successfully"));
+						});
+				});
+
+			Task.WaitAll(tasksToWait.ToArray());
+		}
+
+		private static Action CreateProcessingAction(
+			bool completesSuccessfully,
+			TimeSpan processingTime)
+		{
+			return () =>
+			{
+				Console.WriteLine("Start action processing..");
+
+				Thread.Sleep(processingTime);
+				if (!completesSuccessfully)
+				{
+					throw new ApplicationException("task processing exception");
+				}
+			};
+		}
+	}
+}
